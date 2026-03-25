3c98f87a07a19de7cb49a9c4e234fb6153efcf15
JonyBonyy <balloonboyloveballoons@gmail.com>
2026-03-25 05:10:18 +0200

    Initial Project Setup.

---
+++ JonySub/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace JonySub.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ JonySub/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace JonySub.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
